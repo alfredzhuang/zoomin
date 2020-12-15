8666ad87268bd4ada7db91403f8442687c1dd420
alfredzhuang <alfredzhuang123@gmail.com>
2020-12-14 19:28:39 -0800

    added help function and command responses

--- bot.js
+++ bot.js
@@ -9,4 +9,69 @@ client.on('ready', () => {
 
 })
 
+client.on('message', (msg) => {
+     if(msg.author.bot) {
+         return
+     }
+
+     // commands
+     if(msg.content.startsWith("!")) {
+         command(msg)
+     }
+})
+
+function command(msg) {
+    let full = msg.content.substr(1)
+    let split = full.split(" ")
+    let primary = split[0]
+    let arguments = split.slice(1)
+
+    // help command
+    if(primary == "help") {
+        help(msg)
+    }
+    // else if(primary == "addclass") {
+    //     addclass(arguments, msg)
+    // }
+    // else if(primary == "removeclass") {
+    //     remove(arguments, msg)
+    // }
+    // else if(primary == "addtest") {
+    //     addtest(arguments, msg)
+    // }
+    // else if(primary == "removetest") {
+    //     removetest(arguments, msg)
+    // }
+    // else if(primary == "addquiz") {
+    //     addquiz(arguments, msg)
+    // }
+    // else if(primary == "removequiz") {
+    //     removequiz(arguments, msg)
+    // }
+    // else if(primary == "addhw") {
+    //     addhw(arguments, msg)
+    // }
+    // else if(primary == "removehw") {
+    //     removehw(arguments, msg)
+    // }
+    else {
+        msg.channel.send("Unknown command. Try `!help`")
+    }
+}
+
+function help(msg) {
+    msg.channel.send("I'm not sure what you need help with. Try these commands: \n"
+                    + "`!addclass`, `!removeclass`, `!addtest`, `!removetest`, \n"
+                    + "`!addquiz`, `!removequiz`, `!addhw`, or `!removehw`")
+}
+
+// function addclass(arguments, msg)
+// function removeclass(arguments, msg)
+// function addtest(arguments, msg)
+// function removetest(arguments, msg)
+// function addquiz(arguments, msg)
+// function removequiz(arguments, msg)
+// function addhw(arguments, msg)
+// function removehw(arguments, msg)
+
 client.login(process.env.DISCORD_KEY)
