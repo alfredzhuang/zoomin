1f6c632906276ff43b9e2f25c0f344912d68ee57
alfredzhuang <alfredzhuang123@gmail.com>
2020-12-19 13:37:04 -0800

    refactored the addtest/addclass/addquiz/addhw methods and added validation of date for months

--- commands.js
+++ commands.js
@@ -88,25 +88,8 @@ function code(msg) {
         msg.channel.send("Invalid arguments. Try `!addclass [class name] [zoom link] [day of the week] [time from 0:00-23:59] [Y to notify @everyone, N to notify only yourself]`")
         return
     }
-    else if(arguments[2] != "M" && arguments[2] != "T" && arguments[2] != "W" && arguments[2] != "TH" && arguments[2] != "F") {
-        msg.channel.send("Invalid day, try M-T-W-TH-F") 
-        return
-    }
-    else if(!arguments[3].includes(":")) {
-        msg.channel.send("Invalid time, try between 0:00-23:59")
-        return
-    }
-    else if(arguments[4] != "Y" && arguments[4] != "y" && arguments[4] != "N" && arguments[4] != "n") {
-        msg.channel.send("Invalid Letter, try Y or N")
-        return
-    }
-    let time = arguments[3].split(":")
-    if(time[0] < 0 || time[0] > 23) {
-        msg.channel.send("Invalid time, try between 0:00-23:59")
-        return
-    }
-    else if(time[1] < 0 || time[1] > 59 || time[1].length != 2) {
-        msg.channel.send("Invalid time, try between 0:00-23:59")
+    let time = validateClass(arguments, msg) 
+    if(time == null) {
         return
     }
     let newClass = {
@@ -139,34 +122,38 @@ function code(msg) {
     })
  }
 
+ function validateClass(arguments, msg) {
+    if(arguments[2] != "M" && arguments[2] != "T" && arguments[2] != "W" && arguments[2] != "TH" && arguments[2] != "F") {
+        msg.channel.send("Invalid day, try M-T-W-TH-F") 
+        return null
+    }
+    else if(!arguments[3].includes(":")) {
+        msg.channel.send("Invalid time, try between 0:00-23:59")
+        return null
+    }
+    else if(arguments[4] != "Y" && arguments[4] != "y" && arguments[4] != "N" && arguments[4] != "n") {
+        msg.channel.send("Invalid Letter, try Y or N")
+        return null
+    }
+    let time = arguments[3].split(":")
+    if(time[0] < 0 || time[0] > 23) {
+        msg.channel.send("Invalid time, try between 0:00-23:59")
+        return null
+    }
+    else if(time[1] < 0 || time[1] > 59 || time[1].length != 2) {
+        msg.channel.send("Invalid time, try between 0:00-23:59")
+        return null
+    }
+    return time
+ }
+
  function addTest(arguments, msg) {
     if(arguments.length != 5) {
         msg.channel.send("Invalid arguments. Try `!addtest [class of test] test date [month (1-12)] [date (1-31)] [time from 0:00-23:59] [Y to notify @everyone, N to notify only yourself]`")
         return
     }
-    else if(arguments[1] < 1 || arguments[1] > 12) {
-        msg.channel.send("Invalid month, try between 1-12") 
-        return
-    }
-    else if(arguments[2] < 1 || arguments[2] > 31) {
-        msg.channel.send("Invalid date, try between 1-31")
-        return
-    }
-    else if(!arguments[3].includes(":")) {
-        msg.channel.send("Invalid time, try between 0:00-23:59")
-        return
-    }
-    else if(arguments[4] != "Y" && arguments[4] != "y" && arguments[4] != "N" && arguments[4] != "n") {
-        msg.channel.send("Invalid Letter, try Y or N")
-        return
-    }
-    let time = arguments[3].split(":")
-    if(time[0] < 0 || time[0] > 23) {
-        msg.channel.send("Invalid time, try between 0:00-23:59")
-        return
-    }
-    else if(time[1] < 0 || time[1] > 59 || time[1].length != 2) {
-        msg.channel.send("Invalid time, try between 0:00-23:59")
+    let time = validateArguments(arguments, msg)
+    if(time == null) {
         return
     }
     let newTest = {
@@ -203,29 +190,8 @@ function code(msg) {
         msg.channel.send("Invalid arguments. Try `!addquiz [class of test] quiz date [month (1-12)] [date (1-31)] [time from 0:00-23:59] [Y to notify @everyone, N to notify only yourself]`")
         return
     }
-    else if(arguments[1] < 1 || arguments[1] > 12) {
-        msg.channel.send("Invalid month, try between 1-12") 
-        return
-    }
-    else if(arguments[2] < 1 || arguments[2] > 31) {
-        msg.channel.send("Invalid date, try between 1-31")
-        return
-    }
-    else if(!arguments[3].includes(":")) {
-        msg.channel.send("Invalid time, try between 0:00-23:59")
-        return
-    }
-    else if(arguments[4] != "Y" && arguments[4] != "y" && arguments[4] != "N" && arguments[4] != "n") {
-        msg.channel.send("Invalid Letter, try Y or N")
-        return
-    }
-    let time = arguments[3].split(":")
-    if(time[0] < 0 || time[0] > 23) {
-        msg.channel.send("Invalid time, try between 0:00-23:59")
-        return
-    }
-    else if(time[1] < 0 || time[1] > 59 || time[1].length != 2) {
-        msg.channel.send("Invalid time, try between 0:00-23:59")
+    let time = validateArguments(arguments, msg)
+    if(time == null) {
         return
     }
     let newQuiz = {
@@ -262,29 +228,8 @@ function code(msg) {
         msg.channel.send("Invalid arguments. Try `!addhw [class of test] deadline [month (1-12)] [date (1-31)] [time from 0:00-23:59] [Y to notify @everyone, N to notify only yourself]`")
         return
     }
-    else if(arguments[1] < 1 || arguments[1] > 12) {
-        msg.channel.send("Invalid month, try between 1-12") 
-        return
-    }
-    else if(arguments[2] < 1 || arguments[2] > 31) {
-        msg.channel.send("Invalid date, try between 1-31")
-        return
-    }
-    else if(!arguments[3].includes(":")) {
-        msg.channel.send("Invalid time, try between 0:00-23:59")
-        return
-    }
-    else if(arguments[4] != "Y" && arguments[4] != "y" && arguments[4] != "N" && arguments[4] != "n") {
-        msg.channel.send("Invalid Letter, try Y or N")
-        return
-    }
-    let time = arguments[3].split(":")
-    if(time[0] < 0 || time[0] > 23) {
-        msg.channel.send("Invalid time, try between 0:00-23:59")
-        return
-    }
-    else if(time[1] < 0 || time[1] > 59 || time[1].length != 2) {
-        msg.channel.send("Invalid time, try between 0:00-23:59")
+    let time = validateArguments(arguments, msg)
+    if(time == null) {
         return
     }
     let newHw = {
@@ -315,6 +260,57 @@ function code(msg) {
             return
         }
       })
+ }
+
+ function validateArguments(arguments, msg) {
+    if (arguments[1] < 1 || arguments[1] > 12) {
+        msg.channel.send("Invalid month, try between 1-12") 
+        return null
+    }
+    else if(!arguments[3].includes(":")) {
+        msg.channel.send("Invalid time, try between 0:00-23:59")
+        return null
+    }
+    else if(arguments[4] != "Y" && arguments[4] != "y" && arguments[4] != "N" && arguments[4] != "n") {
+        msg.channel.send("Invalid Letter, try Y or N")
+        return null
+    }
+    if(arguments[1] == 4 || arguments[1] == 6 || arguments[1] == 9 || arguments[1] == 11) {
+        if(arguments[2] < 0 || arguments[2] > 30) {
+            msg.channel.send("Invalid date, there are only 30 days in that month")
+            return null
+        }
+    }
+    else if(arguments[1] == 2) {
+        let d = new Date()
+        let year = d.getFullYear()
+        if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            if(arguments[2] < 0 || arguments[2] > 29) {
+                msg.channel.send("Invalid date, there are only 29 days in that month")
+                return null
+            }
+            else {
+                msg.channel.send("Invalid date, there are only 28 days in that month")
+                return null
+            }
+        }
+    }
+    else {
+        if(arguments[2] < 0 || arguments[2] > 31) {
+            msg.channel.send("Invalid date, there are only 31 days in that month")
+            return null
+        }
+    }
+    let time = arguments[3].split(":")
+    if(time[0] < 0 || time[0] > 23) {
+        msg.channel.send("Invalid time, try between 0:00-23:59")
+        return null
+    }
+    else if(time[1] < 0 || time[1] > 59 || time[1].length != 2) {
+        msg.channel.send("Invalid time, try between 0:00-23:59")
+        return null
+    }
+    return time
  }
 
  function seeClasses(msg) {
